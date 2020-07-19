b47025e85050363df8e0cf0a92dccb9123e16c2f
D.K. Smetters <smetters@gmail.com>
2020-07-19 11:57:34 -0700

    Testing

---
+++ TestingInnerGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestingOuterApp
+//
+//  Created by Diana Smetters on 7/19/20.
+//  Copyright © 2020 SCCLC Swift Fab. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    
+}
+
